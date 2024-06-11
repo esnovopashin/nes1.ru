d8e60b856123516050c358db7c15d4dcee43d1db
esnovopashin <nes2010@yandex.ru>
2024-06-11 16:12:15 +0500

    update

---
+++ dreams.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>NES papercraft. myDreams.</title>
+<?php
+	$title = "NESpapercraft | myDreams";
+	$description = '<META content='.'"Войти-в-ай-ти кому зазо )))'.'" name=DESCRIPTION>';
+	$keywords = '<META content="'.'мечты, думы, рассуждалки'.'" name=keywords>';
+	include'inc/header.php';
+	include'inc/lcol.php';
+	include'pages/dreams.php';
+	include'inc/rcol.php';
+	include'inc/footer.php';
+?>
+</body>
+</html>
